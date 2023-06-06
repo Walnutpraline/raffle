ba866455e4f3474204923f030b9644b6ac74503f
Walnutpraline <2539404838@qq.com>
2023-06-06 17:09:21 +0800

    new

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable camelcase -->
+<template>
+  <div class="home">
+    <xlsx @getResult="getMyExcelData">导入参数</xlsx>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import xlsx from '@/components/xlsx'
+export default {
+  name: 'Home',
+  components: {
+    xlsx
+  },
+  methods: {
+    getMyExcelData (data) {
+      // 上传表格
+      console.log(data)
+    }
+  }
+}
+</script>
